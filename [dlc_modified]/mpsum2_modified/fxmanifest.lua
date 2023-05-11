43ae8a044da46ca2430609883db51495d9f1fe20
mayank <mkmayankmaki2000@gmail.com>
2023-05-11 14:53:48 +0530

    initial commit

---
+++ [dlc_modified]/mpsum2_modified/fxmanifest.lua
@@ -0,0 +1,13 @@
+
+fx_version "bodacious"
+game "gta5"
+author "MayanK"
+
+
+files {
+    'data/*.meta'
+}
+
+-- data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
